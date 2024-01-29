0df81f551ab2e04b1fe4f954bb990fb6ea5e6c0e
Phani <Phani@gmail.com>
2024-01-29 13:16:30 +0530

    commit 1

---
+++ main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    rafay = {
+      source = "RafaySystems/rafay"
+      version = "1.1.22"
+    }
+  }
+}
+
+provider "rafay" {
+  # provider_config_file = "./rafay_config.json"
+}
+
+#Basic example for namespace
+resource "rafay_namespace" "tfdemonamespace" {
+  metadata {
+    name    = var.namespace_name
+    project = var.project_name
+  }
+  spec {
+    drift {
+      enabled = false
+    }
+    placement {
+      labels {
+        key   = "rafay.dev/clusterName"
+        value = var.cluster_name
+      }
+      labels {
+        key   = "rafay.dev/projectName"
+        value = var.project_name
+      }
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "project_name" {
+  type = string
+  default = "team-e"
+}
+
+variable "workspace_admins" {
+  type    = list
+  default = ["phani.kg@gmail.com"]
+}
+
+variable "cluster_name" {
+  type = string
+  default = "multi-eks1"
+}
+
+variable "main_cluster_project_name" {
+  type = string
+  default = "team-a"
+}
+
+variable "namespace_name" {
+  type = string
+  default = "ns1"
+}
